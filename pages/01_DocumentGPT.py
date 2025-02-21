ce04814fda142dfd54ec0e65aa2d7ad604b03fa3
AlpacaMale <alpaca9100@gmail.com>
2025-02-21 17:03:11 +0900

    feat: add rag-based document answering

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,12 +1,22 @@
 from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import TextLoader
+from langchain.document_loaders import TextLoader  # PDFMinerLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.storage import LocalFileStore
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 import streamlit as st
 
+llm = ChatOpenAI(
+    temperature=0.1,
+)
 
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+
+@st.cache_resource(show_spinner="Embedding file..")
 def embed_file(file):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
@@ -20,7 +30,9 @@ def embed_file(file):
         chunk_size=600,
         chunk_overlap=100,
     )
-    loader = TextLoader("./files/chapter_one.txt")
+    if file.name.endswith(".txt"):
+        loader = TextLoader(file_path)
+
     docs = loader.load_and_split(text_splitter=splitter)
     embeddings = OpenAIEmbeddings()
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
@@ -28,6 +40,36 @@ def embed_file(file):
     retriever = vectorstore.as_retriever()
     return retriever
 
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state["messages"].append({"message": message, "role": role})
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(message["message"], message["role"], save=False)
+
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+        Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+
+        Context: {context}
+        """,
+        ),
+        ("human", "{question}"),
+    ]
+)
 
 st.set_page_config(
     page_title="DocumentGPT",
@@ -41,15 +83,33 @@ st.markdown(
 Welcome!
 
 Use this chatbot to ask questions to an AI about your files!
+
+Upload your files on the sidebar.
 """
 )
 
-file = st.file_uploader(
-    "Upload a .txt .pdf or .docx file",
-    type=["pdf", "txt", "docx"],
-)
+with st.sidebar:
+    file = st.file_uploader(
+        "Upload a .txt .pdf or .docx file",
+        type=["pdf", "txt", "docx"],
+    )
 
 if file:
     retriever = embed_file(file)
-    s = retriever.invoke("winston")
-    s
+    send_message("I'm ready! Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your file...")
+    if message:
+        send_message(message, "human")
+        chain = (
+            {
+                "context": retriever | RunnableLambda(format_docs),
+                "question": RunnablePassthrough(),
+            }
+            | prompt
+            | llm
+        )
+        response = chain.invoke(message)
+        send_message(response.content, "ai")
+else:
+    st.session_state["messages"] = []
